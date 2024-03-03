e0a63137705a3f2a6e3dd587bf47fa58164962a8
Christopher Davidson <christopher.davidson@gmail.com>
2024-03-03 16:48:38 -0500

    clean_up_files.sh - made this more concise and clear, easier to update in future mandoc_results.sh - consolidated with "split" program to make it one file instead of 2 split_mandoc_results.sh - removed the workflow and consolidated the items inside of mandoc_results.sh

--- tools/clean_up_files.sh
+++ tools/clean_up_files.sh
@@ -1,6 +1,7 @@
 #!/usr/local/bin/zsh
 
 OUTPUT_DIR="/manpages/output"
+MANDOC_OUTPUT_DIR="/manpages/output/mandoc"
 
 FILE_LIST='broken_base.txt 
               broken_base.txt.bac 
@@ -13,10 +14,26 @@ FILE_LIST='broken_base.txt
               combined_base_refs.txt 
               combined_contrib_refs.txt 
               whatis_results.txt 
-              whatis_results.txt.bac
-              mandoc_style_errors.txt'
+              whatis_results.txt.bac'
+
+MANDOC_FILE_LIST='mandoc_style_errors.txt
+                  mandoc_80characters_error.txt
+                  mandoc_escape_sequence_error.txt
+                  mandoc_newsentence_error.txt
+                  mandoc_pargraph_error.txt
+                  mandoc_fn_error.txt
+                  mandoc_missingtitles_error.txt
+                  mandoc_paragraph_error.txt
+                  mandoc_tabs_error.txt
+                  mandoc_whitespace_error.txt'
 
 echo "Cleaning up the output..."
 for f in $FILE_LIST;  
   do rm -Rf "${HOME}/${OUTPUT_DIR}/${f}" done
 done
+
+echo "Cleaning up the mandoc output.."
+for f in $MANDOC_FILE_LIST;
+  do rm -Rf "${HOME}/${MANDOC_OUTPUT_DIR}/${f}" done
+done
+
--- tools/mandoc_results.sh
+++ tools/mandoc_results.sh
@@ -1,4 +1,52 @@
-#!/bin/sh
+#!/usr/bin/zsh
 
-find /usr/src -name '*.[0-9]' -type f | xargs mandoc -Tlint  > /home/chrisdavidson/manpages/output/mandoc_style_errors.txt
+MANDOC_STYLE="../output/mandoc/mandoc_style_errors.txt"
 
+echo "Deleting previously generated files..."
+if [ -f ../output/mandoc/mandoc_80characters_error.txt ]; then
+  rm ../output/mandoc/mandoc_80characters_error.txt;
+elif [ -f ../output/mandoc/mandoc_fn_error.txt ]; then
+  rm ../output/mandoc/mandoc_fn_error.txt;
+elif [ -f ../output/mandoc/mandoc_missingtitles_error.txt ]; then
+  rm ../output/mandoc/mandoc_missingtitles_error;
+elif [ -f ../output/mandoc/mandoc_newsentence_error.txt ]; then
+  rm ../output/mandoc/mandoc_newsentence_error.txt;
+elif [ -f ../output/mandoc/mandoc_whitespace_error.txt ]; then
+  rm ../output/mandoc/mandoc_whitespace_error.txt;
+elif [ -f ../output/mandoc/mandoc_tabs_error.txt ]; then
+  rm ../output/mandoc/mandoc_tabs_error.txt;
+elif [ -f ../otuput/mandoc_escape_sequence_error.txt ]; then
+  rm ../output/mandoc/mandoc_escape_sequence_error.txt;
+elif [ -f ../output/mandoc/mandoc_paragraph_error.txt ]; then
+  rm ../output/mandoc/mandoc_paragraph_error.txt;
+fi
+
+echo "Finding all the manual pages..."
+find "${HOME}/src/manuals" -name '*.[0-9]' -type f | xargs mandoc -Tlint  > /home/chrisdavidson/manpages/output/mandoc/mandoc_style_errors.txt
+
+echo "Finding entires with more than 80 characters..."
+cat "${MANDOC_STYLE}" | grep "longer than 80" > ../output/mandoc/mandoc_80characters_error.txt
+
+echo "Finding issues with .Fn entries..."
+cat "${MANDOC_STYLE}" | grep "Fn" > ../output/mandoc/mandoc_fn_error.txt
+
+echo "Finding issues with ueless macro .Tn..."
+cat "${MANDOC_STYLE}" | grep "useless macro: Tn" >> ../output/mandoc/mandc_tn_error.txt
+
+echo "Finding missing manual titles.."
+cat "${MANDOC_STYLE}" | grep "missing manual title" > ../output/mandoc/mandoc_missingtitles_error.txt
+
+echo "Finding new sentence style errors.."
+cat "${MANDOC_STYLE}" | grep "new sentence" > ../output/mandoc/mandoc_newsentence_error.txt
+
+echo "Finding whitespace at end of lines.."
+cat "${MANDOC_STYLE}" | grep "whitepsace at end" > ../output/mandoc/mandoc_whitespace_error.txt
+
+echo "Finding tab in filed text errors..."
+cat "${MANDOC_STYLE}" | grep "tab in filled" > ../output/mandoc/mandoc_tabs_error.txt
+
+echo "Finding invalid escape sequence errors..."
+cat "${MANDOC_STYLE}" | grep "invalid escape sequence" > ../output/mandoc/mandoc_escape_sequence_error.txt
+
+echo "Finding invalid paragraph macrco PP after SS.."
+cat "${MANDOC_STYLE}" | grep "skipping paragraph macro" > ../output/mandoc/mandoc_paragraph_error.txt
