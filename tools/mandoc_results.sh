6870145247224124742625cd065fa3738dadc886
Christopher Davidson <christopher.davidson@gmail.com>
2024-03-03 23:31:19 -0500

    mandoc-results.sh - reduced complexity on deleting files run_program.sh - reordered the execution steps to group by logical file manamagenet

--- tools/mandoc_results.sh
+++ tools/mandoc_results.sh
@@ -3,23 +3,7 @@
 MANDOC_STYLE="../output/mandoc/mandoc_style_errors.txt"
 
 echo "Deleting previously generated files..."
-if [ -f ../output/mandoc/mandoc_80characters_error.txt ]; then
-  rm ../output/mandoc/mandoc_80characters_error.txt;
-elif [ -f ../output/mandoc/mandoc_fn_error.txt ]; then
-  rm ../output/mandoc/mandoc_fn_error.txt;
-elif [ -f ../output/mandoc/mandoc_missingtitles_error.txt ]; then
-  rm ../output/mandoc/mandoc_missingtitles_error;
-elif [ -f ../output/mandoc/mandoc_newsentence_error.txt ]; then
-  rm ../output/mandoc/mandoc_newsentence_error.txt;
-elif [ -f ../output/mandoc/mandoc_whitespace_error.txt ]; then
-  rm ../output/mandoc/mandoc_whitespace_error.txt;
-elif [ -f ../output/mandoc/mandoc_tabs_error.txt ]; then
-  rm ../output/mandoc/mandoc_tabs_error.txt;
-elif [ -f ../otuput/mandoc_escape_sequence_error.txt ]; then
-  rm ../output/mandoc/mandoc_escape_sequence_error.txt;
-elif [ -f ../output/mandoc/mandoc_paragraph_error.txt ]; then
-  rm ../output/mandoc/mandoc_paragraph_error.txt;
-fi
+rm ../output/mandoc/*.txt
 
 echo "Finding all the manual pages..."
 find "${HOME}/src/manuals" -name '*.[0-9]' -type f | xargs mandoc -Tlint  > /home/chrisdavidson/manpages/output/mandoc/mandoc_style_errors.txt
@@ -31,7 +15,7 @@ echo "Finding issues with .Fn entries..."
 cat "${MANDOC_STYLE}" | grep "Fn" > ../output/mandoc/mandoc_fn_error.txt
 
 echo "Finding issues with ueless macro .Tn..."
-cat "${MANDOC_STYLE}" | grep "useless macro: Tn" >> ../output/mandoc/mandc_tn_error.txt
+cat "${MANDOC_STYLE}" | grep "useless macro: Tn" >> ../output/mandoc/mandoc_tn_error.txt
 
 echo "Finding missing manual titles.."
 cat "${MANDOC_STYLE}" | grep "missing manual title" > ../output/mandoc/mandoc_missingtitles_error.txt
--- tools/run_program.sh
+++ tools/run_program.sh
@@ -1,10 +1,17 @@
 #!/usr/local/bin/zsh
 
+echo "cleaning up files.."
 sh clean_up_files.sh
+echo "working through mandoc -Tlint..."
+sh mandoc_results.sh
+echo "working through whatis ..."
+perl check_whatis.pl
+echo "parsing manual pages..."
 sh check_pages.sh
+echo "processing results of manual pages.."
 perl parse_broken_refs.pl
+echo "post-processing output files..."
 cp /home/chrisdavidson/manpages/output/broken_refs.txt /home/chrisdavidson/manpages/output/broken_refs.txt.bac
 cat /home/chrisdavidson/manpages/output/broken_refs.txt.bac | tr -s '[:space:]' > /home/chrisdavidson/manpages/output/broken_refs.txt
+echo "combine results..."
 perl combine_outputs.pl
-sh mandoc_results.sh
-perl check_whatis.pl
