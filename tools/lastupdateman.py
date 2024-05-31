9853c0a9aeea9b9c9eec0b4b6f699d9f668100bb
Christopher Davidson <christopher.davidson@gmail.com>
2024-05-30 22:38:25 -0400

    small tweaks

--- tools/lastupdateman.py
+++ tools/lastupdateman.py
@@ -19,7 +19,6 @@ rootdir = "/usr/share/man"
 
 for subdir, dirs, files in os.walk(rootdir):
     for file in files:
-        #print os.path.join(subdir, file)
         filepath = subdir + os.sep + file
 
         if filepath.endswith(".gz"):
