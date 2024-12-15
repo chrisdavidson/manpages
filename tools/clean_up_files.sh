c0c7f6ce18ff069013fed7569dd2ce677d82fb04
Christopher Davidson <christopher.davidson@gmail.com>
2024-12-15 16:09:33 -0500

    Update .gitignore and remove unwanted files

--- tools/clean_up_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-OUTPUT_DIR="/manpages/output"
-MANDOC_OUTPUT_DIR="/manpages/output/mandoc"
-
-echo "Cleaning up the output..."
-find "${HOME}/${OUTPUT_DIR}" -type f -exec rm -rf {} \;
-find "${HOME}/${MANDOC_OUTPUT_DIR}" -type f -exec rm -rf {} \;
